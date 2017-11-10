ac0af0fffd499544c169a306eb080e620edb444e
Denis Policarpo <campos_bra@hotmail.com>
2017-11-10 00:09:10 -0200

    Create model invite

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021172624) do
+ActiveRecord::Schema.define(version: 20171110020633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20171021172624) do
     t.datetime "updated_at", null: false
     t.index ["team_id"], name: "index_channels_on_team_id", using: :btree
     t.index ["user_id"], name: "index_channels_on_user_id", using: :btree
+  end
+
+  create_table "invites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_invites_on_team_id", using: :btree
+    t.index ["user_id"], name: "index_invites_on_user_id", using: :btree
   end
 
   create_table "messages", force: :cascade do |t|
@@ -79,6 +88,8 @@ ActiveRecord::Schema.define(version: 20171021172624) do
 
   add_foreign_key "channels", "teams"
   add_foreign_key "channels", "users"
+  add_foreign_key "invites", "teams"
+  add_foreign_key "invites", "users"
   add_foreign_key "talks", "teams"
   add_foreign_key "team_users", "teams"
   add_foreign_key "team_users", "users"
