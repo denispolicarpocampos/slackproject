4bef482acac846fde463d3e2223449854dc88b86
Denis Policarpo Campos <campos_bra@hotmail.com>
2017-11-10 13:44:57 -0200

    Add mailer and mailer text

--- app/mailers/invite_user_mailer.rb
+++ app/mailers/invite_user_mailer.rb
@@ -1,5 +1,6 @@
 class InviteUserMailer < ApplicationMailer
-
-
-
+  def send_invite(invite)
+     @invite = invite
+     mail(to: @invite.user.email, subject: "Hello you receive a invite for team #{@invite.team.slug} on SlackProject")
+   end
 end
